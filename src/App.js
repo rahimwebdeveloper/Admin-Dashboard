7c855fd1d5852f4004cd781ec73bf23ebf9e4e42
rahimwebdeveloper <info.abdurrahim12@gmail.com>
2022-10-29 19:42:23 +0600

    setting butten add done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 function App() {
+  const activeMenu = true ;
   return (
-    <div className="App">
-      <h1>this is rahim</h1>
-      <p>this is programming </p>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat ad consequuntur quis dolores assumenda adipisci earum aperiam? Quaerat placeat ipsum quia nesciunt natus? Adipisci animi dolores harum sequi repellat perferendis.</p>
+    <div>
+      <div className="flex relative dark:bg-main-dark">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <TooltipComponent content="setting" positions="Top">
+            <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background: "blue", borderRadius: "50%"}} >
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+        { activeMenu ? (
+          <div>
+            sidebar
+          </div>
+        ) : ( 
+          <div>
+            sidebar w-0
+          </div>
+        )
+
+        }
+      </div>
     </div>
-    
   );
 }
 
